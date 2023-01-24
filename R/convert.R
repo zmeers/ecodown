8cf1c82e445367a2da6d62b2c0a67b4acb07f7b6
Zoe Meers <zoe.m@canva.com>
2023-01-24 20:11:26 +1000

    update file tree function

--- R/convert.R
+++ R/convert.R
@@ -107,7 +107,7 @@ ecodown_convert <- function(package_source_folder = "",
   
   all_files <- dir_ls(package_source_folder, recurse = TRUE, type = "file")
   
-  msg_color_title("Copying/Converting to Quarto")
+  msg_color_title("Copying/Converting R Files to Quarto")
   
   pkg <- as_pkgdown(package_source_folder)
   
@@ -117,7 +117,7 @@ ecodown_convert <- function(package_source_folder = "",
     nchar(all_files)
   )
   
-  if(clone_header()) msg_summary_title("Copying/Converting to Quarto")
+  if(clone_header()) msg_summary_title("Copying/Converting R Files to Quarto")
   msg_summary_header()
   msg_summary_entry("|")
   
